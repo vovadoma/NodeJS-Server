4c4639f9c40c64f932bdd644876f777247668a13
vova <vladimir.gubarenko@gmail.com>
2023-05-01 14:11:01 +0300

    fix to module, add test example

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,21 +1,26 @@
-import { afterEach, beforeEach, describe, it, test } from "node:test";
+import { afterEach, beforeEach, describe, it, test } from 'node:test';
 import assert from 'node:assert';
+import { logger as pino } from '../lib/logger.js';
+
 
 
 test('synchronous passing test', (t) => {
+  t.diagnostic('A diagnostic message');
+  assert.strictEqual(1, 1, 'message');
+});
+
+test('asynchronous passing test', async () => {
   assert.strictEqual(1, 1);
 });
 
-test('asynchronous passing test', async (t) => {
-  assert.strictEqual(1, 1);
-});
-
-test('skip option', { skip: true }, (t) => {
+test('skip option', { skip: true }, () => {
+  pino.error({ok: 123});
   assert.strictEqual(1, 2);
 });
 
 describe('tests', async () => {
-  beforeEach(() => console.log('about to run a test'));
+  beforeEach(() => pino.info('about to run a test'));
+  afterEach(() => pino.info('finish'));
   it('is a subtest', () => {
     assert.strictEqual(1, 1);
   });
